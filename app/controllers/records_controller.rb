bdcd151fe31b5e2cf498db5bf1fac62059756e62
tpr1992 <terence.pataneronan@gmail.com>
2019-05-06 19:54:28 -0400

    work on record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,15 @@
 class RecordsController < ApplicationController
+  def index
+    @records = Record.all
+
+    render json: @records
+  end
+
+  def show
+    @record  = Record.find(params[:id])
+
+    render json: @record
+  end
+
+
 end
